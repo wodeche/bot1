f3d2bde3d45268c274270f02b93789a1c20716b9
wodeche <40746051+wodeche@users.noreply.github.com>
2023-04-26 11:42:16 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const myToken = process.env['TOKEN']
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+
+client.login(myToken);
+
