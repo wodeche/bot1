6b13b57a3e876fb565c8ab72655014d0636fd6a2
wodeche <40746051+wodeche@users.noreply.github.com>
2023-04-28 15:32:13 +0000

    my first commmit

---
+++ src/events/ready/index.js
@@ -0,0 +1,9 @@
+import { Events } from 'discord.js'
+export const event = {
+  name: Events.ClientReady,
+  once: true
+}
+
+export const action = (c) => {
+  console.log(`Ready!Logged in ${c.user.tag}`)
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 const myToken = process.env['TOKEN']
 import { Client, Events, GatewayIntentBits } from 'discord.js';
+import vueinit from '@/core/vue'
+import { loadCommands, loadEvents } from '@/core/loader'
+import { useAppStore } from '@/store/app'
+
+vueinit()
+
+loadCommands()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const appStore = useAppStore()
+appStore.client = client
+loadEvents()
 
-client.once(Events.ClientReady, c => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+// client.once(Events.ClientReady, c => {
+//   console.log(`Ready! Logged in as ${c.user.tag}`);
+// });
 
 
 client.login(myToken);
